4e96dcd61de5e69178e7c6e702bcff3f6864e4ec
Alex <alexukrok@gmail.com>
2026-01-23 19:56:37 +0200

    Added second language to main handler

--- bot/MainHandler.py
+++ bot/MainHandler.py
@@ -1,39 +1,41 @@
-from pickle import FROZENSET
-
 from aiogram import F, Router
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, CallbackQuery
 from Keyboards import *
 from Database.requests import get_user_language
+from MainTexts import MAIN_MK_TEXT
 router = Router()
+
+
+
+async def get_text(tg_id, text_details, user_lang):
+    lang_from_db = await get_user_language(tg_id)
+    final_lang = lang_from_db if lang_from_db else user_lang
+
+    if final_lang == "ru":
+        final_lang = "uk"
+
+    if final_lang not in ["en", "uk"]:
+        final_lang = "en"
+
+    target_dict = MAIN_MK_TEXT.get(text_details, {})
+    return target_dict.get(final_lang, target_dict.get(final_lang, "Error: Text not found"))
+
+
+
 
 
 @router.message(CommandStart())
 async def start(message: Message):
-    await message.reply(f"<b>Hello user!</b>\n"
-                        f"<b>Please enter <code>Register</code> to fully register and get as personalized plan as possible</b>\n"
-                        f"Or just proceed without registration."
-                        f"few commands you might need:"
-                        f"1. Choose 'Info' button to get tutorial trough bot\n"
-                        f"2. Choose 'Trainings' to get your personalized plan\n"
-                        f"3. Choose 'Music' to get playlists based on your mood\n",
+    await message.reply( await get_text(message.from_user.id,"Greetings", message.from_user.language_code),
                         reply_markup=mainENkb,
                         parse_mode='HTML')
 
 
 @router.message(F.text == "ℹ️ Info")
 async def info(message: Message):
-    await message.reply(
-        "<b>🦾 AI Training Assistant</b>\n\n"
-        "<blockquote>Your personalized coach that listens to your mood. This app generates and adjusts training programs based on how you feel right now.</blockquote>\n\n"
-        "<b>How it works:</b>\n"
-        "1️⃣ Open <b>Trainings</b> in the main menu\n"
-        "2️⃣ Select your <b>target muscle group</b>\n"
-        "3️⃣ Describe your <b>mood & energy level</b>\n\n"
-        "✨ <i>The AI will instantly craft a structured plan tailored just for you.</i>\n\n"
-        "💎<i>If you want experience our bot's best abilities please complete registration by sending: <code>Register</code> to the bot and answering few questions</i>\n"
-        "⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯\n"
-        "<b>Powered by:</b> <a href='https://github.com/Alexukk'>Alexukk</a>",
+    await message.reply(await get_text(message.from_user.id,"Info", message.from_user.language_code)
+        ,
         parse_mode='HTML',
         disable_web_page_preview=True
     )
---
+++ bot/MainTexts.py
@@ -0,0 +1,41 @@
+MAIN_MK_TEXT = {
+    "Greetings": {
+        "uk": (f"<b>👋 Привіт, друже!</b>\n\n"
+               f"<b>🤖 Будь ласка, введіть <code>Register</code>, щоб пройти повну реєстрацію та отримати максимально персоналізований план 📈</b>\n\n"
+               f"Або просто продовжуйте без реєстрації. Ось кілька команд, які можуть вам знадобитися:\n\n"
+               f"1️⃣ Натисніть кнопку <b>'Info'</b> ℹ️, щоб переглянути туторіал\n"
+               f"2️⃣ Натисніть <b>'Trainings'</b> 💪, щоб отримати свій персональний план\n"
+               f"3️⃣ Натисніть <b>'Music'</b> 🎵, щоб отримати плейлисти під свій настрій\n"),
+
+        "en": (f"<b>👋 Hello user!</b>\n\n"
+               f"<b>🤖 Please enter <code>Register</code> to fully register and get as personalized plan as possible 📈</b>\n\n"
+               f"Or just proceed without registration. Few commands you might need:\n\n"
+               f"1️⃣ Choose <b>'Info'</b> button ℹ️ to get tutorial\n"
+               f"2️⃣ Choose <b>'Trainings'</b> 💪 to get your personalized plan\n"
+               f"3️⃣ Choose <b>'Music'</b> 🎵 to get playlists based on your mood\n")
+    },
+
+    "Info": {
+        "uk": (f"<b>🦾 AI Training Assistant</b>\n\n"
+               f"<blockquote>Ваш персональний коуч, який прислухається до вашого настрою. Ця програма створює та адаптує тренування на основі того, як ви почуваєтеся саме зараз.</blockquote>\n\n"
+               f"<b>Як це працює:</b>\n"
+               f"1️⃣ Відкрийте <b>Trainings</b> у головному меню\n"
+               f"2️⃣ Оберіть <b>цільову групу м'язів</b>\n"
+               f"3️⃣ Опишіть свій <b>настрій та рівень енергії</b>\n\n"
+               f"✨ <i>Штучний інтелект миттєво створить структурований план спеціально для вас.</i>\n\n"
+               f"💎 <i>Якщо ви хочете випробувати найкращі можливості бота, будь ласка, пройдіть реєстрацію, надіславши: <code>Register</code> та відповівши на кілька запитань.</i>\n"
+               f"⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯\n"
+               f"<b>Powered by:</b> <a href='https://github.com/Alexukk'>Alexukk</a>"),
+
+        "en": (f"<b>🦾 AI Training Assistant</b>\n\n"
+               f"<blockquote>Your personalized coach that listens to your mood. This app generates and adjusts training programs based on how you feel right now.</blockquote>\n\n"
+               f"<b>How it works:</b>\n"
+               f"1️⃣ Open <b>Trainings</b> in the main menu\n"
+               f"2️⃣ Select your <b>target muscle group</b>\n"
+               f"3️⃣ Describe your <b>mood & energy level</b>\n\n"
+               f"✨ <i>The AI will instantly craft a structured plan tailored just for you.</i>\n\n"
+               f"💎 <i>If you want to experience our bot's best abilities please complete registration by sending: <code>Register</code> and answering a few questions.</i>\n"
+               f"⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯\n"
+               f"<b>Powered by:</b> <a href='https://github.com/Alexukk'>Alexukk</a>")
+    }
+}
